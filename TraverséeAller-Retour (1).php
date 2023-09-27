f275d68742960e9d37073c41e0ab1d44ee69bc3a
JorisLADET <144709863+JorisLADET@users.noreply.github.com>
2023-09-27 13:43:25 +0200

    Add files via upload

---
+++ TraverséeAller-Retour (1).php
@@ -0,0 +1,14 @@
+<?php
+	
+	echo "Saisir le nom du port de départ : " ;
+
+	$portDepart = fgets( STDIN )  ;
+	
+	echo "Saisir le nom du port d'arrivée : " ;
+	
+	$portArrivee = fgets( STDIN )  ;
+	
+	echo $portDepart , $portArrivee , "\n";
+ 
+    
+?>
